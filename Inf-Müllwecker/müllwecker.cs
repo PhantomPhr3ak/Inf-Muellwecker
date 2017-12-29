bbcb4949e9e44335d5338820a9da0ea95ca2416c
PhantomPhr3ak <34191989+PhantomPhr3ak@users.noreply.github.com>
2017-12-30 00:40:25 +0100

    Build Class Structure
    Created most of the necessary methods and attributes of the class

--- Inf-Müllwecker/müllwecker.cs
+++ Inf-Müllwecker/müllwecker.cs
@@ -6,8 +6,63 @@ using System.Threading.Tasks;
 
 namespace Inf_Müllwecker
 {
-    class müllwecker
+    class Müllwecker
     {
+        /* Farbe ID's:
+         * 1 = Blau
+         * 2 = Braun
+         * 3 = Gelb
+         * 4 = Rot
+         * 5 = Grau
+         */
 
+        //Attribute
+        private int[] id = new int[] { }; 
+        private DateTime[] datum = new DateTime[] { };
+        private int[] farbID = new int[] {};
+
+
+
+        //Grundfunktionen 
+        void lesen()
+        {
+            
+        }
+
+        void schreiben()
+        {
+            
+        }
+
+
+
+        //Daten manipulieren
+        void neuerEintrag()
+        {
+            
+        }
+
+        void eintragLöschen()
+        {
+            
+        }
+
+
+
+        //Attribute auslesen
+        int[] getID()
+        {
+            return id;
+        }
+
+        DateTime[] getDatum()
+        {
+            return datum;
+        }
+
+        int[] getFarbID()
+        {
+            return farbID;
+        }
     }
 }
