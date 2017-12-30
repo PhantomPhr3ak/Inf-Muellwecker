b5e730b00857cec2bfacfdabb605c13faad1a97e
PhantomPhr3ak <34191989+PhantomPhr3ak@users.noreply.github.com>
2017-12-30 10:52:40 +0100

    implemented reading and writing into textfile

--- Inf-Müllwecker/müllwecker.cs
+++ Inf-Müllwecker/müllwecker.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Inf_Müllwecker
 {
@@ -16,22 +18,55 @@ namespace Inf_Müllwecker
          * 5 = Grau
          */
 
+        //letzterEintrag bezieht sich auf die ID
+
         //Attribute
         private int[] id = new int[] { }; 
         private DateTime[] datum = new DateTime[] { };
         private int[] farbID = new int[] {};
-
+        private int letzterEintrag;
 
 
         //Grundfunktionen 
         void lesen()
         {
-            
+            string line;
+            letzterEintrag = 0;
+            string lineID = null;
+            string lineFarbe = null;
+            string lineDatum = null;
+
+            //Daten abrufen
+            StreamReader reader = new StreamReader(@"C:\müllweckerSpeicher.txt");
+            for (int i = 0; (line = reader.ReadLine()) != null; i++)
+            {
+                //Splitting the string into 3 parts (id, datum, farbe) and deleting the semicolons
+                lineID = line.Substring(0, Convert.ToInt32(line.IndexOf(";"))-1);
+                id[i] = Convert.ToInt32(lineID);
+
+                line = line.Substring(Convert.ToInt32(line.IndexOf(";")), line.Length+1);
+
+                lineDatum = line.Substring(0, Convert.ToInt32(line.IndexOf(";")) - 1);
+                datum[i] = Convert.ToDateTime(lineDatum);
+
+                line = line.Substring(Convert.ToInt32(line.IndexOf(";")), line.Length+1);
+
+                lineFarbe = line;
+                farbID[i] = Convert.ToInt32(lineFarbe);
+            }
+            reader.Close();
         }
 
         void schreiben()
         {
-            
+            //Daten schreiben
+            StreamWriter writer = new StreamWriter(@"C:\müllweckerSpeicher.txt");
+            for (int i = 0; i < letzterEintrag; i++)
+            {
+                //String zusammensetzen um die Daten Zeilenweise auslesen zu können
+                writer.WriteLine(id[i] + ";" + datum[i] + ";" + farbID[i]);
+            }
+            writer.Close();
         }
 
 
